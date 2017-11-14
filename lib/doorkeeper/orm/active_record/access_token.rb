4be9b798f40ef5eb1670668aab346f6c250881ee
Yunus Bahari <yunus@bukalapak.com>
2017-11-14 17:21:40 +0700

    change into secondbase

--- lib/doorkeeper/orm/active_record/access_token.rb
+++ lib/doorkeeper/orm/active_record/access_token.rb
@@ -1,5 +1,5 @@
 module Doorkeeper
-  class AccessToken < ActiveRecord::Base
+  class AccessToken < SecondBase::Base
     self.table_name = "#{table_name_prefix}oauth_access_tokens#{table_name_suffix}".to_sym
 
     include AccessTokenMixin
